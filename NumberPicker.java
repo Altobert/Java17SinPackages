4d835baa0051d22385e607532b07861134607a96
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-06-03 17:40:07 -0400

    se agregan cosas

--- NumberPicker.java
+++ NumberPicker.java
@@ -1,10 +1,11 @@
-import java.util.Random; // EL import le dice donde encontrar la clase Random
+//import java.util.Random; // EL import le dice donde encontrar la clase Random
+import java.util.*; // EL import le dice donde encontrar la clase Random
 public class NumberPicker{
 	public static void main(String [] args){
 		Random r  = new Random();
 		// lanza un numero entre cero y nueve
 		// como los arreglos java comienza contando con cero.
-		|0|-|1|-|2|-|3|-|4|-|5|-|6|-|7|-|8|-|9|
+		//|0|-|1|-|2|-|3|-|4|-|5|-|6|-|7|-|8|-|9|
 		System.out.println(r.nextInt(10));
 	}
 }
