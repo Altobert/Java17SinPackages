0531bd08dd3b6a4b497cbddf83d68bd91b2db0fe
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-06-30 11:10:58 -0400

    se agrega una prueba de string un poco mas complejo

---
+++ temp/teststring/TestString.java
@@ -0,0 +1,13 @@
+
+package teststring;
+
+public class TestString {
+    
+
+    public static void main(String [] args){
+
+        String eyeTest ="\"Java study guide\"\n  by Scott and Jeane";
+        System.out.println(eyeTest);
+
+    }
+}
