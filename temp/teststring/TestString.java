31960f495de67c43f3ae422df9dc1d6ac6a426d4
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-06-30 15:35:50 -0400

    se agrega lo necesario

--- temp/teststring/TestString.java
+++ temp/teststring/TestString.java
@@ -3,11 +3,19 @@ package teststring;
 
 public class TestString {
     
-
     public static void main(String [] args){
 
         String eyeTest ="\"Java study guide\"\n  by Scott and Jeane";
         System.out.println(eyeTest);
+
+        //Java tiene los bloques de texto
+        //que son una forma de crear cadenas de texto multilínea
+        String blockTest ="""
+                "Java study guide"
+                    by Scott and Jeane
+                """;
+        System.out.println(blockTest);
+
 
     }
 }
