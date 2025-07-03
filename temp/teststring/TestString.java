24815abaf654bb81cf9bd5e60b640cdb88f30ab7
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-07-03 15:42:58 -0400

    se agregan ejemplos de valores para un string y los escapes

--- temp/teststring/TestString.java
+++ temp/teststring/TestString.java
@@ -2,11 +2,11 @@
 package teststring;
 
 public class TestString {
-    
+
     public static void main(String [] args){
 
         String eyeTest ="\"Java study guide\"\n  by Scott and Jeane";
-        System.out.println(eyeTest);
+        System.out.println(eyeTest+"\n");
 
         //Java tiene los bloques de texto
         //que son una forma de crear cadenas de texto multilínea
@@ -14,22 +14,37 @@ public class TestString {
                 "Java study guide"
                     by Scott and Jeane
                 """;
-        System.out.println(blockTest);
-		
+        System.out.println(blockTest+"\n");
+
 		String piramid="""
 		  *
 		 * *
 		* * *
 		""";
-		
-		System.out.println(piramid);
+
+		System.out.println(piramid+"\n");
 
 
         String blooString="""
                 doe\
                 deer
                 """;
-        System.out.println(blooString);
+        System.out.println(blooString+"\n");
+
+
+        String miString ="""
+        saludos
+        """;
+        System.out.println("*"+miString+"*\n");
+
+
+        String block = """
+        	"doeeee\"\"\"
+        	\"deeeeer\"""
+
+        	""";
+
+        System.out.println("*"+block+"*");
 
 
     }
