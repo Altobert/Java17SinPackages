209d7964db177941efc06984ab3cde635412dda9
Altobert <alberto.sanmartin@usach.cl>
2025-07-12 13:25:08 -0400

    tipo inferencia para variable local

---
+++ temp/varsample/VarKeyboard.java
@@ -0,0 +1,7 @@
+package varsample;
+
+public class VarKeyboard{
+  var tricky="Hello";
+  // No compila por que no se permite le keyboard var
+  // en el lugar donde se declaran las variables de instancia.
+}
