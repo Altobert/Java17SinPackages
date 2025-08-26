711786e53ac292cd7c162cabd237d00f2fae3754
Altobert <alberto.sanmartin@usach.cl>
2025-08-25 21:50:00 -0400

    se agrega shell

---
+++ unixCompile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Compila todos los archivos .java en subdirectorios de temp/
+cd temp
+find . -name "*.java" > sources.txt
+if [ -s sources.txt ]; then
+    javac -d classes -sourcepath . -classpath ../libs/* @sources.txt
+    java -cp classes algoritmos.Mcd 7 11
+    java -cp classes structure.Meerkat
+    echo "Compilación y ejecución completada."
+else
+    echo "No se encontraron archivos .java en temp/"
+fi
+rm -f sources.txt
