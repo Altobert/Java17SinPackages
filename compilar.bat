5dc93b4dc725de36a993530adcac5bf69d6abd5e
Alberto San Martin Concha <alberto.sanmartin@sii.cl>
2025-09-08 11:04:17 -0300

    se comenta .bat

--- compilar.bat
+++ compilar.bat
@@ -8,7 +8,15 @@ rem Compilando
 javac -d classes packagea/*.java packageb/*.java packagec/*.java algoritmos/*.java structure/*.java swanexample/*.java teststring/*.java varsample/*.java
 
 rem Ejecutando
-java -cp classes algoritmos.Mcd 7 11
+java -cp classes packagea.ClassA
+java -cp classes packageb.ClassB
+::java -cp classes packagec.ClassC
+java -cp classes swanexample.Swan
+java -cp classes teststring.TestString
+::java -cp classes varsample.VarKeyboard
+java -cp classes varsample.Zoo 10 10
+::java -cp classes orderinit.Egg
+java -cp classes algoritmos.Mcd 10 10
 java -cp classes structure.Meerkat
 
 endlocal
