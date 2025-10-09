7efe1eee072ca981a9c11112d8e5cee1c5c21011
Altobert <alberto.sanmartin@usach.cl>
2025-10-08 21:42:50 -0300

    se corrigen alert

--- unixCompile.sh
+++ unixCompile.sh
@@ -7,7 +7,7 @@ if [ -s sources.txt ]; then
     javac -d classes -sourcepath . -classpath ../libs/* @sources.txt
     java -cp classes algoritmos.Mcd 7 11
     java -cp classes structure.Meerkat
-    java -cp classes practicalaberinto.GraphExample
+    # java -cp classes practicalaberinto.GraphExample
     echo "Compilación y ejecución completada."
 else
     echo "No se encontraron archivos .java en temp/"
