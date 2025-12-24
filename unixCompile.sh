0af4019d530670cc1344403a11d84baabe7c32aa
Altobert <alberto.sanmartin@usach.cl>
2025-12-23 23:23:54 -0300

    var type inference

--- unixCompile.sh
+++ unixCompile.sh
@@ -4,9 +4,12 @@
 cd temp
 find . -name "*.java" > sources.txt
 if [ -s sources.txt ]; then
+    # se compilan todas las clases y se ejecutan algunas de ellas
     javac -d classes -sourcepath . -classpath ../libs/* @sources.txt
     java -cp classes algoritmos.Mcd 7 11
     java -cp classes structure.Meerkat
+    #java -cp classes typeinference.PileOfPapersToFileInFilingCabinet
+    java -cp classes typeinference.TesTypeInference
     # java -cp classes practicalaberinto.GraphExample
     echo "Compilación y ejecución completada."
 else
